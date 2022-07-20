1507137991ac3adee58a7ff9ed7160e6bb80abad
lchoudhary <leena@gmail.com>
2022-07-20 15:40:56 +0530

    Updated

--- src/com/training/MyClass.java
+++ src/com/training/MyClass.java
@@ -3,7 +3,7 @@ package com.training;
 public class MyClass {
 
 	public static void main(String[] args) {
-		System.out.println("hello world...welcome to the jenkins learning");
+		System.out.println("hello leena...welcome to the jenkins learning");
 
 	}
 
